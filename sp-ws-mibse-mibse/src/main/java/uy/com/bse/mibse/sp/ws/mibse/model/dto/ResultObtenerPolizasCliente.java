7086ca3097fa229cbad8ed7071e41ac3484ab3e5
Andrés Aguirre <aaguirre@pyxis.tech>
2024-09-09 14:40:44 -0300

    resolviendo merge conflict

--- sp-ws-mibse-mibse/src/main/java/uy/com/bse/mibse/sp/ws/mibse/model/dto/ResultObtenerPolizasCliente.java
+++ sp-ws-mibse-mibse/src/main/java/uy/com/bse/mibse/sp/ws/mibse/model/dto/ResultObtenerPolizasCliente.java
@@ -1,6 +1,7 @@
 package uy.com.bse.mibse.sp.ws.mibse.model.dto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import uy.com.bse.mibse.sp.ws.mibse.utilitario.dato.ResultGenerico;
 
@@ -8,6 +9,7 @@ import uy.com.bse.mibse.sp.ws.mibse.utilitario.dato.ResultGenerico;
 public class ResultObtenerPolizasCliente extends ResultGenerico {
     private static final long serialVersionUID = -566111179456801184L;
 
+    //TODO: aaguirre revisar si List o ArrayList
     ArrayList<DatosFacturacion> datosFacturacion = new ArrayList<DatosFacturacion>();
 
     public ArrayList<DatosFacturacion> getDatosFacturacion() {
